f9bae73d3ccf99846f0c11f54bc62277dfd521f6
AdrianGuevara <adrian.guevara.ds@gmail.com>
2019-05-23 10:33:44 -0500

    refactor: decorated max and min score search with depth cutting

--- chess-proyect.py
+++ chess-proyect.py
@@ -1,37 +1,43 @@
 from math import inf
 
-def best_possible_move(board, depth):
-    return (move for move in board.legal_moves if score(board_after_move(move, board)) == max_play(board, -inf, inf, 0))[0]
+def best_move(board, depth=8):
+    best_move_yet, highest_score_yet = None, -inf
+    for move, board in successors(board):
+        best_move_yet, highest_score_yet = max((best_move_yet, highest_score_yet), (move, min_value(board, depth-1, highest_score_yet, inf)), key=lambda x: x[1])
+    return best_move_yet
 
-def board_after_move(move, board):
-    new_board = board.copy(false)
-    new_board.push(move)
-    return new_board
+def score_search_with_cut(score_search):
+    def wrapper(board, depth, *args):
+        return score(board) if depth == 0 or game_finished(board) else score_search(board, depth, *args)
+    return wrapper;
 
-def max_play(play, alpha, beta, depth):
-    if depth == 0 or game_finished(play.board):
-        return play, score(play.board)
+@score_search_with_cut
+def max_score(board, depth, alpha, beta):
     value = -inf
-    for successor in successors(play.board):
-        play, value = max((play, value), min_play(successor, alpha, beta, depth-1), key=lambda x: x[1])
+    for move, board in successors(board):
+        value = max(value, min_score(board, depth-1, alpha, beta))
         if value >= beta:
-            return play, value
+            return value
         alpha = max(alpha, value)
-    return play, value
+    return value
 
-def min_play(play, alpha, beta, depth):
-    if depth == 0 or game_finished(play.board):
-        return play, score(play.board)
-    v = inf
-    for successor in successors(play.board):
-        play, value = min((play, value), max_play(successor, alpha, beta, depth-1), key=lambda x: x[1])
+@score_search_with_cut
+def min_score(board, depth, alpha, beta):
+    value = inf
+    for move, board in successors(board):
+        value = min(value, max_score(board, depth-1, alpha, beta))
         if value <= alpha:
-            return play, value
+            return value
         beta = min(beta, value)
     return play, value
 
 def successors(board):
     return (move, board_after_move(move, board) for move in board.legal_moves)
+
+def board_after_move(move, board):
+    new_board = board.copy(False)
+    new_board.push(move)
+    return new_board
 
 def score(board):
     return 1
