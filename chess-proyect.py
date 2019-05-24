8a040ba9533e19ac6c4dc14df752bac6f94cf0a3
AdrianGuevara <adrian.guevara.ds@gmail.com>
2019-05-24 10:48:04 -0500

    refactor: PIECE_TYPE_VALUES and UNICODE_PIECE_SYMBOLS moved to local scope

--- chess-proyect.py
+++ chess-proyect.py
@@ -92,7 +92,7 @@ UNICODE_PIECE_SYMBOLS = {
     "/": "|\n",
 }
 
-def board_to_unicode(board):
+def unicode(board):
     return my_strtr(board.fen(), UNICODE_PIECE_SYMBOLS)
 
 board = chess.Board()
@@ -102,7 +102,7 @@ while not board.is_game_over():
     move = best_move(board, 5)
     board.push(move)
     print(move)
-    print(board_to_unicode(board))
+    print(unicode(board))
     turn+=1
     print(board.attacks(9))
 if board.is_checkmate():
