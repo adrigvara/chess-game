5631b7d6438e1d51836da331026ef596041e991b
AdrianGuevara <adrian.guevara.ds@gmail.com>
2019-05-25 22:06:31 -0500

    feature: space heuristic added

--- chess-proyect.py
+++ chess-proyect.py
@@ -1,16 +1,28 @@
+from statistics import mean
 from math import inf
-import chess
+from chess import *
 
-def best_move(board, depth=6):
+player_color = None
+
+
+def best_move(board, depth=5):
+    global player_color
+    player_color = board.turn
     best_move_yet, highest_score_yet = None, -inf
     for move, board in successors(board):
-        best_move_yet, highest_score_yet = max((best_move_yet, highest_score_yet), (move, min_score(board, depth-1, highest_score_yet, inf)), key=lambda x: x[1])
+        best_move_yet, highest_score_yet = max(
+            (best_move_yet, highest_score_yet),
+            (move, min_score(board, depth-1, highest_score_yet, inf)),
+            key=lambda x: x[1])
     return best_move_yet
+
 
 def score_search_with_cut(score_search):
     def wrapper(board, depth, *args):
-        return score(board) if depth == 0 or board.is_game_over() else score_search(board, depth, *args)
-    return wrapper;
+        return score(board) if depth == 0 or board.is_game_over() \
+            else score_search(board, depth, *args)
+    return wrapper
+
 
 @score_search_with_cut
 def max_score(board, depth, alpha, beta):
@@ -22,6 +34,7 @@ def max_score(board, depth, alpha, beta):
         alpha = max(alpha, value)
     return value
 
+
 @score_search_with_cut
 def min_score(board, depth, alpha, beta):
     value = inf
@@ -32,86 +45,84 @@ def min_score(board, depth, alpha, beta):
         beta = min(beta, value)
     return value
 
+
 def successors(board):
     return ((move, board_after_move(move, board)) for move in board.legal_moves)
+
 
 def board_after_move(move, board):
     new_board = board.copy()
     new_board.push(move)
     return new_board
 
+
 def score(board):
-    return result_value(board) if board.is_game_over() else material_heuristic(board)
+    return result_value(board) if board.is_game_over() else heuristic_score(board)
+
 
 def result_value(board):
-    return 0 if draw(board.result()) else 1 if win(board.result(), board.turn) else -1
+    return 0 if draw(board.result()) else 1 if win(board.result()) else -1
+
 
 def draw(result):
-    return result=="1/2-1/2"
+    return result == "1/2-1/2"
 
-def win(result, color):
-    return (result=="1-0" and color==chess.WHITE) or (result=="0-1" and color==chess.BLACK)
 
-def material_heuristic(board):
-    white_material = material_value(board, chess.WHITE)
-    black_material = material_value(board, chess.BLACK)
-    return (white_material - black_material) if board.turn else (black_material - white_material)/(white_material + black_material)
+def win(result):
+    return (result == "1-0" and player_color == WHITE) or \
+        (result == "0-1" and player_color == BLACK)
 
-def material_value(board, color):
+
+def heuristic_score(board):
+    return mean((material_heuristic(board), space_heuristic(board)))
+
+
+def heuristic(heuristic_func):
+    def wrapper(board):
+        return heuristic_value(heuristic_func(board, player_color), heuristic_func(board, not player_color))
+
+    def heuristic_value(player_value, opponent_value):
+        return (player_value - opponent_value) / (player_value + opponent_value)
+    return wrapper
+
+
+@heuristic
+def material_heuristic(board, color):
     PIECE_TYPE_VALUES = {
-        chess.PAWN: 1,
-        chess.KING: 1.7,
-        chess.KNIGHT: 2.5,
-        chess.BISHOP: 3,
-        chess.ROOK: 4.7,
-        chess.QUEEN: 7.7,
+        KING:   20000,
+        QUEEN:  900,
+        ROOK:   500,
+        BISHOP: 330,
+        KNIGHT: 320,
+        PAWN:   100,
     }
-    return sum(PIECE_TYPE_VALUES[piece.piece_type] for piece in board.piece_map().values() if piece.color == color)
+    return sum(PIECE_TYPE_VALUES[piece.piece_type] for piece in board.piece_map().values()
+               if piece.color == color)
 
-def unicode(board):
-    UNICODE_PIECE_SYMBOLS = {
-        "r": u"|♖", "R": u"|♜",
-        "n": u"|♘", "N": u"|♞",
-        "b": u"|♗", "B": u"|♝",
-        "q": u"|♕", "Q": u"|♛",
-        "k": u"|♔", "K": u"|♚",
-        "p": u"|♙", "P": u"|♟",
-        "1": "| ",
-        "2": "| | ",
-        "3": "| | | ",
-        "4": "| | | | ",
-        "5": "| | | | | ",
-        "6": "| | | | | | ",
-        "7": "| | | | | | | ",
-        "8": "| | | | | | | | ",
-        "/": "|\n",
-    }
-    return my_strtr(board.fen(), UNICODE_PIECE_SYMBOLS)
 
-def my_strtr(cadena, reemplazo):
-    import re
-    regex = re.compile("(%s)" % "|".join(map(re.escape, reemplazo.keys())))
-    return regex.sub(lambda x: str(reemplazo[x.string[x.start() :x.end()]]), cadena)
+@heuristic
+def space_heuristic(board, color):
+    return sum(len(board.attacks(square)) for square, piece
+               in board.piece_map().items() if piece.color == color)
 
-board = chess.Board()
+
+game = Board()
 turn = 1
-while not board.is_game_over():
-    print("turn", turn, "white" if board.turn else "black", "material:", material_heuristic(board))
-    move = best_move(board, 5)
-    board.push(move)
-    print(move)
-    print(board_to_unicode(board))
-    turn+=1
-    print(board.attacks(9))
-if board.is_checkmate():
+while not game.is_game_over():
+    print(game.unicode(invert_color=True))
+    move = best_move(game, 4)
+    print("Turn:", turn, "White:" if player_color else "Black:", move)
+    game.push(move)
+    turn += 1
+if game.is_checkmate():
     print("checkmate")
-if board.is_stalemate():
+if game.is_stalemate():
     print("stalemate")
-if board.is_insufficient_material():
+if game.is_insufficient_material():
     print("insufficient material")
-if board.is_seventyfive_moves():
+if game.is_seventyfive_moves():
     print("is seventyfive moves")
-if board.is_fivefold_repetition():
+if game.is_fivefold_repetition():
     print("fivefold repetition")
-if board.is_variant_end():
+if game.is_variant_end():
     print("variant end")
