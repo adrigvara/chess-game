81cb9576868d1bef1da07ac768bbb32bfe66bc7e
AdrianGuevara <adrian.guevara.ds@gmail.com>
2019-05-27 13:37:52 -0500

    feature: piece-square heuristic added

--- chess-proyect.py
+++ chess-proyect.py
@@ -2,12 +2,12 @@ from statistics import mean
 from math import inf
 from chess import *
 
-player_color = None
+ai_color = None
 
 
-def best_move(board, depth=5):
-    global player_color
-    player_color = board.turn
+def best_move(board, depth=4):
+    global ai_color
+    ai_color = board.turn
     best_move_yet, highest_score_yet = None, -inf
     for move, board in successors(board):
         best_move_yet, highest_score_yet = max(
@@ -69,17 +69,18 @@ def draw(result):
 
 
 def win(result):
-    return (result == "1-0" and player_color == WHITE) or \
-        (result == "0-1" and player_color == BLACK)
+    return (result == "1-0" and ai_color == WHITE) or \
+        (result == "0-1" and ai_color == BLACK)
 
 
 def heuristic_score(board):
-    return mean((material_heuristic(board), space_heuristic(board)))
+    return sum(heuristic_weight*heuristic_value(board) for heuristic_value, heuristic_weight
+               in HEURISTIC_TABLE.items())
 
 
 def heuristic(heuristic_func):
     def wrapper(board):
-        return heuristic_value(heuristic_func(board, player_color), heuristic_func(board, not player_color))
+        return heuristic_value(heuristic_func(board, ai_color), heuristic_func(board, not ai_color))
 
     def heuristic_value(player_value, opponent_value):
         return (player_value - opponent_value) / (player_value + opponent_value)
@@ -106,23 +107,130 @@ def space_heuristic(board, color):
                in board.piece_map().items() if piece.color == color)
 
 
-game = Board()
-turn = 1
-while not game.is_game_over():
-    print(game.unicode(invert_color=True))
-    move = best_move(game, 4)
-    print("Turn:", turn, "White:" if player_color else "Black:", move)
-    game.push(move)
-    turn += 1
-if game.is_checkmate():
-    print("checkmate")
-if game.is_stalemate():
-    print("stalemate")
-if game.is_insufficient_material():
-    print("insufficient material")
-if game.is_seventyfive_moves():
-    print("is seventyfive moves")
-if game.is_fivefold_repetition():
-    print("fivefold repetition")
-if game.is_variant_end():
-    print("variant end")
+@heuristic
+def piece_square_heuristic(board, color):
+    return sum(piece_square_value(piece.piece_type, square, color)
+               for square, piece in board.piece_map().items()
+               if piece.color == color)
+
+
+def piece_square_value(piece_type, square, color):
+    return PIECE_SQUARE_TABLES[piece_type][square] \
+        if color == WHITE else \
+        PIECE_SQUARE_TABLES[piece_type][::-1][square]
+
+
+PAWN_SQUARE_TABLE = (
+    0,  0,   0,   0,   0,   0,   0,   0,
+    50, 50,  50,  50,  50,  50,  50,  50,
+    10, 10,  20,  30,  30,  20,  10,  10,
+    5,  5,  10,  25,  25,  10,   5,   5,
+    0,  0,   0,  20,  20,   0,   0,   0,
+    5, -5, -10,   0,   0, -10,  -5,   5,
+    5, 10,  10, -20, -20,  10,  10,   5,
+    0,  0,   0,   0,   0,   0,   0,   0,
+)
+KNIGHT_SQUARE_TABLE = (
+    -50, -40, -30, -30, -30, -30, -40, -50,
+    -40, -20,  0,  0,  0,  0, -20, -40,
+    -30,  0, 10, 15, 15, 10,  0, -30,
+    -30,  5, 15, 20, 20, 15,  5, -30,
+    -30,  0, 15, 20, 20, 15,  0, -30,
+    -30,  5, 10, 15, 15, 10,  5, -30,
+    -40, -20,  0,  5,  5,  0, -20, -40,
+    -50, -40, -30, -30, -30, -30, -40, -50,
+)
+BISHOP_SQUARE_TABLE = (
+    -20, -10, -10, -10, -10, -10, -10, -20,
+    -10,  0,  0,  0,  0,  0,  0, -10,
+    -10,  0,  5, 10, 10,  5,  0, -10,
+    -10,  5,  5, 10, 10,  5,  5, -10,
+    -10,  0, 10, 10, 10, 10,  0, -10,
+    -10, 10, 10, 10, 10, 10, 10, -10,
+    -10,  5,  0,  0,  0,  0,  5, -10,
+    -20, -10, -10, -10, -10, -10, -10, -20,
+)
+ROOK_SQUARE_TABLE = (
+    0,  0,  0,  0,  0,  0,  0,  0,
+    5, 10, 10, 10, 10, 10, 10,  5,
+    -5,  0,  0,  0,  0,  0,  0, -5,
+    -5,  0,  0,  0,  0,  0,  0, -5,
+    -5,  0,  0,  0,  0,  0,  0, -5,
+    -5,  0,  0,  0,  0,  0,  0, -5,
+    -5,  0,  0,  0,  0,  0,  0, -5,
+    0,  0,  0,  5,  5,  0,  0,  0,
+)
+QUEEN_SQUARE_TABLE = (
+    -20, -10, -10, -5, -5, -10, -10, -20,
+    -10,   0,   0,  0,  0,   0,   0, -10,
+    -10,   0,   5,  5,  5,   5,   0, -10,
+    -5,   0,   5,  5,  5,   5,   0,  -5,
+    0,   0,   5,  5,  5,   5,   0,  -5,
+    -10,   5,   5,  5,  5,   5,   0, -10,
+    -10,   0,   5,  0,  0,   0,   0, -10,
+    -20, -10, -10, -5, -5, -10, -10, -20,
+)
+KING_SQUARE_TABLE = (
+    -30, -40, -40, -50, -50, -40, -40, -30,
+    -30, -40, -40, -50, -50, -40, -40, -30,
+    -30, -40, -40, -50, -50, -40, -40, -30,
+    -30, -40, -40, -50, -50, -40, -40, -30,
+    -20, -30, -30, -40, -40, -30, -30, -20,
+    -10, -20, -20, -20, -20, -20, -20, -10,
+    20,  20,   0,   0,   0,   0,  20,  20,
+    20,  30,  10,   0,   0,  10,  30,  20,
+)
+KING_SQUARE_TABLE_END_board = (
+    -50, -40, -30, -20, -20, -30, -40, -50,
+    -30, -20, -10,  0,  0, -10, -20, -30,
+    -30, -10, 20, 30, 30, 20, -10, -30,
+    -30, -10, 30, 40, 40, 30, -10, -30,
+    -30, -10, 30, 40, 40, 30, -10, -30,
+    -30, -10, 20, 30, 30, 20, -10, -30,
+    -30, -30,  0,  0,  0,  0, -30, -30,
+    -50, -30, -30, -30, -30, -30, -30, -50,
+)
+PIECE_SQUARE_TABLES = {
+    KING:   KING_SQUARE_TABLE,
+    QUEEN:  QUEEN_SQUARE_TABLE,
+    ROOK:   ROOK_SQUARE_TABLE,
+    BISHOP: BISHOP_SQUARE_TABLE,
+    KNIGHT: KNIGHT_SQUARE_TABLE,
+    PAWN:   PAWN_SQUARE_TABLE,
+}
+HEURISTIC_TABLE = {
+    material_heuristic: 0.75,
+    space_heuristic: 0.25,
+    # piece_square_heuristic: 0.0,
+}
+
+
+def play_chess(player_color, depth):
+    turn = 1
+    board = Board()
+    while not board.is_game_over():
+        print(board.unicode(invert_color=True))
+        move = next_move(board, player_color, depth)
+        print("Turn:", turn, "White:" if ai_color else "Black:", move)
+        board.push(move)
+        turn += 1
+    print(board.unicode(invert_color=True))
+    if board.is_checkmate():
+        print("checkmate")
+    if board.is_stalemate():
+        print("stalemate")
+    if board.is_insufficient_material():
+        print("insufficient material")
+    if board.is_seventyfive_moves():
+        print("seventyfive moves")
+    if board.is_fivefold_repetition():
+        print("fivefold repetition")
+    if board.is_variant_end():
+        print("variant end")
+
+
+def next_move(board, player_color, depth):
+    return input("player move:") if player_color == board.turn else best_move(board, depth)
+
+
+play_chess(None, 4)
